6d5733f1c88c2dcf7f62bcae3a3c25c608801eaa
kasdewolf <kas.dewolf@student.hu.nl>
2024-05-21 11:06:30 +0200

    Update tools.js

--- controllers/tools.js
+++ controllers/tools.js
@@ -33,3 +33,26 @@ exports.getAllProducts = (req, res) => {
     });
 };
 
+exports.deleteTool = (req, res) => {
+    const { id } = req.params;
+    const query = 'DELETE FROM tools WHERE id = ?';
+
+    console.log('ID ontvangen voor verwijdering:', id); // Voeg deze regel toe
+
+    db.query(query, [id], (error, results) => {
+        if (error) {
+            console.error('Fout bij het verwijderen van product:', error);
+            return res.status(500).send('Er is een interne serverfout opgetreden bij het verwijderen van het product');
+        }
+
+        // Controleer het aantal rijen dat werd beïnvloed door de query
+        if (results.affectedRows === 0) {
+            console.log('Geen rijen verwijderd. Controleer of de ID bestaat:', id);
+            return res.status(404).send('Geen product gevonden met de opgegeven ID');
+        }
+
+        console.log('Product succesvol verwijderd. Aantal rijen beïnvloed:', results.affectedRows);
+
+        res.status(200).send({ message: 'Product succesvol verwijderd' });
+    });
+}
