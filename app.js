d7114304e7e4aa00ff0e1c24d53b94d015a56b76
kasdewolf <kas.dewolf@student.hu.nl>
2024-05-29 11:23:50 +0200

    app.js verbeterd

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mysql = require("mysql");
-const hbs = require('handlebars');
+const handlebars = require('handlebars'); // Wijzig de naam naar `handlebars` om verwarring te voorkomen
 const dotenv = require('dotenv');
 const db = require('./db');
-const hbs = require('hbs');
-require('dotenv').config()
+const hbs = require('hbs'); // Dit is correct
+require('dotenv').config();
 const path = require('path');
 const app = express();
 const toolsRouter = require('./routes/tools');
@@ -17,15 +17,15 @@ app.use(express.static(publicDirectory));
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 
-//zorgt ervoor dat je data van forms kan pakken
+// Zorgt ervoor dat je data van forms kan pakken
 app.use(express.urlencoded({ extended: false }));
-//zorg ervoor dat data als json komt
+// Zorg ervoor dat data als JSON komt
 app.use(express.json());
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
-// Define routes
+// Definieer routes
 app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
 app.use('/tools', toolsRouter);
