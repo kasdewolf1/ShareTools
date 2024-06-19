7dcc36965969bf4b8fb93c3a7bdc9ede7c4c19e0
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2024-06-19 13:47:43 +0200

    veranderingen verwijderd

--- routes/tools.js
+++ routes/tools.js
@@ -7,7 +7,6 @@ router.get('/products', toolsController.getAllProducts);
 router.get('/product/:id', toolsController.getToolById);
 router.delete('/:id', toolsController.deleteTool);
 router.post('/addTool', toolsController.addTool);
-router.get('/toolbewerken/:id', toolsController.getToolByIdForEdit);
-router.post('/updateTool/:id', toolsController.updateTool);
+
 
 module.exports = router;
