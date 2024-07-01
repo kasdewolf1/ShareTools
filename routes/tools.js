1d0580d885ee403a32d441bf1516e665546e2c63
kasdewolf <kas.dewolf@student.hu.nl>
2024-07-01 10:33:31 +0200

    ja

--- routes/tools.js
+++ routes/tools.js
@@ -3,14 +3,9 @@ const router = express.Router();
 const toolsController = require('../controllers/tools');
 const upload = require('../multerConfig'); // Pas het pad aan indien nodig
 
-// Routes definities
+router.post('/', upload.single('image'), toolsController.addTool);
 router.get('/products', toolsController.getAllProducts);
 router.get('/product/:id', toolsController.getToolById);
 router.delete('/:id', toolsController.deleteTool);
-router.post('/addTool', toolsController.addTool);
-router.get('/bewerken/:id', toolsController.getToolByIdForEdit);
-router.post('/editToolById/:id', toolsController.editToolById);
-router.get('/tools/:id', toolsController.getToolById);
-
 
 module.exports = router;
